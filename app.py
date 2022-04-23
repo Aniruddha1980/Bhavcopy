b5d1ede95a7b3030408a0835ae81ecd055189f47
Aniruddha Thakur <102941069+Aniruddha1980@users.noreply.github.com>
2022-04-24 01:01:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -421,6 +421,23 @@ if __name__ == '__main__':
         st.write('Market Status: ',market_time)
     with col3 :
         st.write(date_stamp)
+        
+    c1,c2,c3,c4,c5,c6,c7,c8 = st.columns(8)
+    # c9,c10,c11,c12,c13 = st.columns(5)
+    
+    with c1:
+        nifty =  nse.get_indices(IndexSymbol.Nifty50)
+        st.write('NIFTY50')
+        st.write(nifty['last'][0])
+        st.write(nifty['percentChange'][0],'%')
+        st.write(nifty['variation'][0])
+
+    with c2:
+        niftybank =  nse.get_indices(IndexSymbol.NiftyBank)
+        st.write('NIFTYBANK')
+        st.write(niftybank['last'][0])
+        st.write(niftybank['percentChange'][0],'%')
+        st.write(niftybank['variation'][0])
 
 st.header(selected_analysis)
 analysis_dict[selected_analysis]()
